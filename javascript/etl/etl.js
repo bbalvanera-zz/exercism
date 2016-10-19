e3a0bd8c53a90989b9d2ce64d702e9ae8e03c3f9
Bernie <bernardo.balvanera@gmail.com>
2016-10-19 01:23:59 -0500

    [javascript] adds the etl exercise

---
+++ javascript/etl/etl.js
@@ -0,0 +1,13 @@
+"use strict";
+class ETL {
+    transform(old) {
+        let retVal = {};
+        let scores = Object.keys(old);
+        scores.forEach(score => {
+            let letters = old[score];
+            letters.map(letter => retVal[letter.toLowerCase()] = parseInt(score));
+        });
+        return retVal;
+    }
+}
+module.exports = ETL;
---
+++ javascript/etl/etl.ts
@@ -0,0 +1,15 @@
+class ETL {
+    public transform(old) {
+        let retVal = {};
+        let scores = Object.keys(old) as string[];
+
+        scores.forEach(score => {
+            let letters = old[score] as string[];
+            letters.map(letter => retVal[letter.toLowerCase()] = parseInt(score));
+        });
+
+        return retVal;
+    }
+}
+
+export = ETL;
